a6ac234afe520993955edc39e398f75017691719
guorong wu <daniel.stillpixels@gmail.com>
2024-12-21 15:38:05 +0000

    base hbs created

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const hbs = require('hbs');
+const wax = require('wax-on');
 
 let app = express();
 
 // 1B. SETUP VIEW ENGINE
 app.set('view engine', 'hbs'); 
+
+// 1C. SETUP STATIC FOLDER
+app.use(express.static('public'));
+
+// 1D. SETUP WAX ON (FOR TEMPLATE INHERITANCE)
+wax.on(hbs.handlebars);
+wax.setLayoutPath('./views/layouts')
+
+app.get('/', function(req, res){
+    res.render('index.hbs');
+})
 
 // add routes here 
 app.get('/', function(req,res){
